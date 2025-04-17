6a7bb8188f8e3e1316a8b15dae9416ca3852be7d
juber131 <jubayerahmad131@gmail.com>
2025-04-17 13:45:02 +0600

    add second

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.8dssgfd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 console.log(process.env.DB_USER)
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+  
     await client.connect();
 
 const protfoliocollection = client.db("pictech").collection("protfolio")
@@ -73,7 +73,7 @@ app.get('/paginationcount', async(req,res)=>{
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
+   
     // await client.close();
   }
 }
@@ -82,7 +82,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('Hello  developer!')
+  res.send('Hello welcome come back  developer!')
 })
 
 app.listen(port, () => {
